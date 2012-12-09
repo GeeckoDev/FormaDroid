910241a74d8bf10bebc0a6521e7bc6a8751d3ebb
Clément G <geecko.dev@free.fr>
2012-12-09 11:07:43 +0100

    Code de base pour les parametres dynamiques

--- src/org/geeckodev/formadroid/activities/Preference.java
+++ src/org/geeckodev/formadroid/activities/Preference.java
@@ -5,19 +5,33 @@ import org.geeckodev.formadroid.R;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
 
 public class Preference extends PreferenceActivity {
+	private void setEntries(CharSequence pref, CharSequence[] entries, CharSequence[] values) {
+		ListPreference lp = (ListPreference)findPreference(pref);
+		lp.setEntries(entries);
+		lp.setEntryValues(values);
+	}
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		this.getApplicationContext();
-		SharedPreferences prefs = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
+		/* Indicate that the preferences were opened at least once */
+		
+		SharedPreferences prefs = getSharedPreferences("MyPreferences",
+				Context.MODE_PRIVATE);
 		SharedPreferences.Editor ed = prefs.edit();
 		ed.putBoolean("HaveShownPrefs", true);
 		ed.commit();
-
+		
+		/* Inflate from XML and dynamically create the attributes */
+		
 		this.addPreferencesFromResource(R.xml.preferences);
+		this.setEntries("estts_pref", new CharSequence[]{}, new CharSequence[]{});
+		this.setEntries("depts_pref", new CharSequence[]{}, new CharSequence[]{});
+		this.setEntries("groups_pref", new CharSequence[]{}, new CharSequence[]{});
 	}
 }
