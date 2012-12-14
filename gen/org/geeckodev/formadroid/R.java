3564c8d579d00dd0ac775b569cf67b39d94ea165
Clément G <geecko.dev@free.fr>
2012-12-14 20:00:25 +0100

    texte et spinner alignes

--- gen/org/geeckodev/formadroid/R.java
+++ gen/org/geeckodev/formadroid/R.java
@@ -50,7 +50,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f060000;
         public static final int menu_settings=0x7f060001;
-        public static final int selectedGroup=0x7f060002;
+        public static final int selected_group=0x7f060002;
         public static final int update=0x7f060003;
     }
     public static final class style {
